13472e93e990f7225cee76288425cd2bddb729ee
marufhasan34 <YOUR_GITHUB_EMAIL>
2026-05-03 09:58:42 +0600

    all products page added

--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -1,9 +1,17 @@
+import ProductCard from "@/components/ProductCard";
 
 const ProductsPage =async () => {
     const res = await fetch('https://sun-cart-lac.vercel.app/data.json')
+    const products = await res.json()
+    console.log(products)
     return (
         <div>
-           
+            <h2 className="text-center font-bold text-2xl mb-5">All Products</h2>
+           <div className="grid gap-8 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+            {
+                products.map(product => <ProductCard product={product} key={product.id} />)
+            }
+           </div>
         </div>
     );
 };
